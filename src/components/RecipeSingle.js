b3f661df09b120a84cee57742662bf32b4001ecd
silmu <svetlana.raitina@gmail.com>
2022-05-17 14:34:34 +0300

    Refactor RecipeSingle with useLocation

--- src/components/RecipeSingle.js
+++ src/components/RecipeSingle.js
@@ -1,36 +1,13 @@
-import React, { useState, useEffect, useLocation } from 'react';
-import axios from 'axios';
+import { useLocation } from 'react-router-dom';
 
-const RecipeSingle = (props) => {
-  const [isLoading, setIsLoading] = useState(false);
-  const [data, setData] = useState([]);
-  // const location = useLocation();
+const RecipeSingle = () => {
+  const location = useLocation();
+  const data = location.state.data;
 
-  useEffect(() => {
-    const id = props.params.recipe[0];
-    console.log(id);
-
-    setIsLoading(true);
-    axios
-      .get(`http://localhost:3010/recipes/${id}`)
-      .then((res) => {
-        setData(res.data);
-        setIsLoading(false);
-        // console.log(res.data);
-        // console.log(location);
-      })
-      .catch((err) => {
-        console.log('Axios error: ', err);
-      });
-  }, []);
-
-  if (isLoading) {
-    return <p>Loading...</p>;
-  }
   return (
     <div className="recipeSingle">
       <div className="leftContainer">
-        <img src={data.image}></img>
+        <img src={data.image} alt={data.name}></img>
         <h2>Ingredients:</h2>
         {data.ingredients?.map((ingredient) => {
           return (
